bde0ad3c184a084ce0d1e73475df39762af92b73
Zoltan Toth <ztoth@indigo.ca>
2017-11-09 16:21:42 -0500

    typo fixed

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,7 +2,7 @@ import { types } from '../config/constants';
 
 export default function courseReducer(state = [], action) {
     switch (action.type) {
-        case types.actions.CREATE_COURSE:
+        case types.CREATE_COURSE:
             return [...state, Object.assign({}, action.course)];
         
         default:
