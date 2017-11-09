768bc614b9f987c777651c4e385e39789ac3cf6b
Zoltan Toth <ztoth@indigo.ca>
2017-11-09 10:50:55 -0500

    index page, home + about components and routes wired up

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>About</h1>
+                <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
