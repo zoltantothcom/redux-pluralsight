2991f13a256890da32214f22d61423708210303a
Zoltan Toth <ztoth@indigo.ca>
2017-11-10 14:42:52 -0500

    ManageCoursePage added

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -11,9 +11,9 @@ export function loadCoursesSuccess(courses) {
 export function loadCourses() {
     return function(dispatch) {
         return courseApi.getAllCourses().then(courses => {
-            dispatch(loadCoursesSuccess(courses))
+            dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
             throw(error);
         });
-    }
+    };
 }
