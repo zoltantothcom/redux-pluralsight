bec0ee194d4ef98c707982192dc3ce869eb7edbe
Zoltan Toth <ztoth@indigo.ca>
2017-11-13 15:19:15 -0500

    initial states extracted into separate config file

--- src/reducers/authorReducer.js
+++ src/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import { types } from '../config/constants';
+import initialState from '../config/initialState';
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
     switch (action.type) {
         case types.LOAD_AUTHORS_SUCCESS:
             return action.authors;
