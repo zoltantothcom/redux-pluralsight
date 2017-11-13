f37f1e1d38c3359b8cedf2bf4a26c2ac52149307
Zoltan Toth <ztoth@indigo.ca>
2017-11-13 15:11:16 -0500

    author action creator added

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,26 +2,44 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context);
+
+        this.state = {
+            course: Object.assign({}, props.course),
+            errors: {}
+        };
     }
 
     render() {
         return (
-            <h1>Manage course</h1>
+            <CourseForm 
+                allAuthors={[]}
+                errors={this.state.errors}
+                course={this.state.course} />
         );
     }
 }
 
 ManageCoursePage.propTypes = {
-
-}
+    course: PropTypes.object.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
+    let course = {
+        id: '',
+        watchHref: '',
+        title: '',
+        authorId: '',
+        length: '',
+        category: ''
+    };
+
     return {
-        state: state
+        course: course
     };
 }
 
